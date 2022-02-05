54a7d685243c033bd13c993bdded4b6bcb8fcbdd
Alex Kuzmin <kuzmin@kreitai.com>
2022-02-05 22:08:38 +0800

    Add mock registration function

--- src/app/dapp-injector/classes/provider.ts
+++ src/app/dapp-injector/classes/provider.ts
@@ -15,22 +15,22 @@ export class AngularNetworkProvider {
   constructor(public url_array: Array<string>) {}
 
   async init() {
-    if(this.url_array.length == 0){
-      this._provider = new providers.JsonRpcProvider()
+    if (this.url_array.length == 0) {
+      this._provider = new providers.JsonRpcProvider();
     } else {
-    const p_race = await Promise.race(
-      this.url_array.map((map) => new providers.JsonRpcProvider(map))
-    );
-    this._provider = await p_race;
+      const p_race = await Promise.race(
+        this.url_array.map((map) => new providers.JsonRpcProvider(map))
+      );
+      this._provider = await p_race;
     }
-   
+
     this._signer = this._provider.getSigner();
     return this._provider;
   }
 
   get Provider() {
     if (this._provider == undefined) {
-      throw new Error('Provider not yet initialyzed');
+      throw new Error('Provider not yet initialized');
     }
     return this._provider;
   }
@@ -42,25 +42,22 @@ export class AngularNetworkProvider {
     return this._signer;
   }
 
-  
-  async doTransaction(tx:any) {
- 
-    let notification_message:ITRANSACTION_RESULT = {
-      success: false
-    }
+  async doTransaction(tx: any) {
+    let notification_message: ITRANSACTION_RESULT = {
+      success: false,
+    };
 
-    let transaction_details:ITRANSACTION_DETAILS = {
-      txhash: "",
+    let transaction_details: ITRANSACTION_DETAILS = {
+      txhash: '',
       bknr: 0,
-      from: "",
-      gas: "",
-      to: "",
-      value: "",
+      from: '',
+      gas: '',
+      to: '',
+      value: '',
     };
     try {
-
-       let tx_obj = await this._signer.sendTransaction(tx);
-       let tx_result = await tx_obj.wait();
+      let tx_obj = await this._signer.sendTransaction(tx);
+      let tx_result = await tx_obj.wait();
 
       const result = tx_result;
       transaction_details.txhash = result.transactionHash;
@@ -68,15 +65,13 @@ export class AngularNetworkProvider {
       transaction_details.to = result.to;
       transaction_details.gas = result.gasUsed.toString();
       transaction_details.bknr = result.blockNumber;
-     
-      tx_obj.value == undefined
-        ? (transaction_details.value = "0")
-        : (transaction_details.value = tx_obj.value.toString());
-        notification_message.success = true;
-        notification_message.success_result = transaction_details;
-   
-    } catch (e:any) {
 
+      tx_obj.value == undefined
+        ? (transaction_details.value = '0')
+        : (transaction_details.value = tx_obj.value.toString());
+      notification_message.success = true;
+      notification_message.success_result = transaction_details;
+    } catch (e: any) {
       // console.log(e);
       // Accounts for Metamask and default signer on all networks
       let myMessage =
@@ -103,14 +98,11 @@ export class AngularNetworkProvider {
         //ignore
       }
 
-    
-     notification_message.error_message = myMessage;
-    
+      notification_message.error_message = myMessage;
     }
 
-    return notification_message
-     }
-
+    return notification_message;
+  }
 
   initBlockSubscription() {
     this._provider.on('block', async (log, event) => {
